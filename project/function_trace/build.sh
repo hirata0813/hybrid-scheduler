8de19217fc835f5caea0b7e24a50d26c5a35a493
hirata0813 <hirata2024@s.okayama-u.ac.jp>
2026-07-13 10:35:34 +0900

    launch_function.cc の print 処理を変更

---
+++ project/function_trace/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -eu
+g++ -O0 -o launch_function.out launch_function.cc
--- project/function_trace/launch_function.cc
+++ project/function_trace/launch_function.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <cstdlib>
+#include <sys/syscall.h>
 
 unsigned long long fibonacci(int n) {
     if (n <= 1) {
@@ -10,23 +11,18 @@ unsigned long long fibonacci(int n) {
 }
 
 int main(int argc, char *argv[]) {
-
-    // get the pid of the current process
-    std::string pid = std::to_string(getpid());
-    // Add the process to enclave_1
-    std::string command = "echo " + pid + " > /sys/fs/ghost/enclave_1/tasks";
-
-    int ret = std::system(command.c_str());
-    if (ret == 0) {
-        // if successful, print the command
-        std::cout << command << std::endl;
-    } else {
-        // if failed, print the error message
-        std::cout << "Failed to add to enclave_1" << std::endl;
-    }
-
     int arg = atoi(argv[1]);
+    int idx = atoi(argv[2]);
+
+    pid_t tid = syscall(SYS_gettid);
+
     unsigned long long n = fibonacci(arg);
-    std::cout << n << std::endl;
+
+    std::cout
+        << "idx=" << idx
+        << " tid=" << tid
+        << " n=" << arg
+        << std::endl;
+
     return 0;
 }
